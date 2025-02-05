6e03c6c890d0458d6d915b0a79dc87a8adb53f76
17768 Programmer 1 <17768P1@gmail.com>
2025-02-04 19:53:48 -0500

    nolan took noahs intake stuff and readded it but will make it again

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -5,41 +5,37 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
-
-public class IntakeCommand extends Command {
+/** An example command that uses an example subsystem. */
+public class ExampleCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final IntakeSubsystem m_subsystem;
-  private final CommandXboxController m_controller;
+  private final ExampleSubsystem m_subsystem;
 
-
-  public IntakeCommand(IntakeSubsystem subsystem, CommandXboxController controller) {
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ExampleCommand(ExampleSubsystem subsystem) {
     m_subsystem = subsystem;
-    m_controller = controller;
-
+    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
+  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    if(m_controller.getLeftTriggerAxis() > 0.1)
-    {
-      m_subsystem.setMotorSpeed(0.5);
-    }
-  }
+  public void execute() {}
 
-
+  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
-
+  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
